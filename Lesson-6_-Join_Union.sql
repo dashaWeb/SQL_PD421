f811cd308a17d5ce12a55b3f1e43927df14246e4
Dasha <dashakonopelko@gmail.com>
2025-02-21 16:47:12 +0200

    join

---
+++ Lesson-6_-Join_Union.sql
@@ -0,0 +1,72 @@
+﻿insert into Students ([Name], Surname, Email, Birthdate) values ('Gabriele', 'Streater', 'gstreater0@businessinsider.com', '2012-03-24');
+insert into Students ([Name], Surname, Email, Birthdate) values ('Reid', 'Rivenzon', 'rrivenzon1@istockphoto.com', '2004-06-02');
+insert into Students ([Name], Surname, Email, Birthdate) values ('Dody', 'Brauns', 'dbrauns2@home.pl', '2007-01-13');
+insert into Students ([Name], Surname, Email, Birthdate) values ('Melany', 'Brocklesby', 'mbrocklesby3@sfgate.com', '2003-06-26');
+insert into Students ([Name], Surname, Email, Birthdate) values ('Elliot', 'Peron', 'eperon4@flickr.com', '2014-05-31');
+select * from Students
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Groups]
+from Students as s inner join Groups as g on g.Id = s.GroupId 
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Groups]
+from Students as s left join Groups as g on g.Id = s.GroupId 
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Groups]
+from Students as s left join Groups as g on g.Id = s.GroupId 
+where s.GroupId is null
+
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Groups]
+from Students as s right join Groups as  g on g.Id = s.GroupId
+
+select * from Groups
+insert into Groups
+values ('Ukraine')
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Groups]
+from Students as s right join Groups as  g on g.Id = s.GroupId
+where s.GroupId is null
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Groups]
+from Students as s full outer join Groups as  g on g.Id = s.GroupId
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Groups]
+from Students as s full outer join Groups as  g on g.Id = s.GroupId
+where s.GroupId is null
+
+-- union - обєднує декілька запитів в одну результуючу таблицю при цьому видаляючи дублікати 
+
+select 'Students Count', COUNT(Id)
+from Students
+union
+select 'AverageMark', AVG(AverageMark)
+from Students
+
+
+select Id, Surname
+from Students
+union
+select Id, Name
+from Teachers
+
+select Id, Name
+from Students
+where Name like 'S%'
+--order by Name
+union
+select Id, Name
+from Teachers
+where Name like 'S%'
+order  by Name
+
+
+-- union all - обєднує декілька запитів в одну результуючу таблицю при цьому  дублікати не видаляючи
+
+select Id, Surname
+from Students
+where AverageMark >= 10
+union all
+select Id, Surname
+from Students
+where AverageMark >= 7
+order by Surname
---
+++ Lesson-6_-Subqueries.sql
@@ -0,0 +1,119 @@
+﻿use UniversityPD_421
+
+select* from Students
+select * from Teachers
+select * from Groups
+select* from TeachersGroups
+select Name, Email, AverageMark
+from Students
+where AverageMark = (
+				select Max(AverageMark)
+				from Students)
+
+select g.Name, Avg(AverageMark), COUNT(s.Id)
+from Students as s join Groups as g on s.GroupId = g.Id
+group by g.Name
+having COUNT(s.Id) = (
+	select top 1 COUNT(s.Id)
+	from Students as s join Groups as g on s.GroupId = g.Id
+	group by g.Name
+	order by COUNT(s.Id) desc
+)
+
+
+
+
+select g.Name, Avg(AverageMark), COUNT(s.Id)
+from Students as s join Groups as g on s.GroupId = g.Id
+group by g.Name
+having COUNT(s.Id) = (
+	select top 1 COUNT(s.Id)
+	from Students as s join Groups as g on s.GroupId = g.Id
+	group by g.Name
+	order by COUNT(s.Id) desc
+)
+or COUNT(s.Id) =(
+		select top 1 COUNT(s.Id)
+		from Students as s join Groups as g on s.GroupId = g.Id
+		group by g.Name
+		order by COUNT(s.Id) 
+	)
+
+	select Id, Name, Birthdate
+	from Students
+	where Id IN(select Id 
+				from Students
+				where Name like 'C%')
+
+select t.Id, t.Name as [Teachers], AVG(s.AverageMark) as [Mark], Count(s.Id) as [Students]
+from Teachers as t join TeachersGroups as tg on t.Id = tg.TeacherId
+				join Groups as g on tg.GroupId = g.Id
+				join Students as s on g.Id = s.GroupId
+group by t.Name, t.Id
+having COUNT(s.Id) / 2 < (
+			select COUNT(s.Id)
+				from Teachers as t2 join TeachersGroups as tg on t.Id = tg.TeacherId
+				join Groups as g on tg.GroupId = g.Id
+				join Students as s on g.Id = s.GroupId
+				where s.AverageMark >= 5 and t.Id = t2.Id
+) 
+
+
+/*
+	Subquery Operators
+		- [NOT] EXISTS - повертає true якщо запит повернув хоча б один запис
+		- [> < >= <= <> = ] ANY/SOME - повертає true якщо хоча б один запис відповідає умові
+		- [> < >= <= <> = ] ALL - повертає true якщо всі записи відповідають умові
+*/
+
+
+select Name
+from Groups
+where EXISTS (
+	select Id 
+	from Students
+	where AverageMark = 11.7 and GroupId = Groups.Id
+)
+
+
+select Name, Phone
+from Teachers
+where EXISTS (
+	select s.Id
+	from Students as s join Groups as g on s.GroupId = g.Id
+						join TeachersGroups as tg on g.Id = tg.GroupId
+						where tg.TeacherId = Teachers.Id and s.Name = 'Zorina'
+)
+
+select g.Name
+from Groups as g
+where EXISTS (
+	select s.Id
+		from Students as s
+		where s.GroupId = g.Id and DATEDIFF(YEAR, s.Birthdate, GETDATE()) >= 20
+)
+
+
+select Name, Birthdate, Email
+from Students
+where Birthdate > ANY (select HireDate from Teachers)
+
+select Name, Email, GroupID
+from Students
+where Name = ANY (select s.Name from Students as s
+					where s.GroupId <> Students.GroupId)
+
+select s.Name, s.Email, s.AverageMark, g.Name
+from Groups as g join Students as s on s.GroupId = g.Id
+where AverageMark > ALL (select AverageMark 
+							from Groups as g join Students as s on s.GroupId = g.Id
+							where g.Name = 'Delaware' and s.AverageMark is not null)
+
+select Name, Phone, HireDate
+from Teachers
+where HireDate < ALL (
+		select s.Birthdate
+		from Students as s join Groups as g on s.GroupId = g.Id
+				join TeachersGroups as tg on tg.GroupId = g.Id
+				where tg.TeacherId = Teachers.Id				
+)
